23a7bd9bd97b239dd7c6d1563c587eb316f3964d
hxskmx <hxskmx@163.com>
2015-02-01 18:24:03 +0800

    add cookie

--- src/main/java/cn/com/lc/common/utils/CommonUtils.java
+++ src/main/java/cn/com/lc/common/utils/CommonUtils.java
@@ -1,5 +1,27 @@
 package cn.com.lc.common.utils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 开发工具类
+ * 
+ * @author Administrator
+ *
+ */
 public class CommonUtils {
 
+	public static String getCookieValue(HttpServletRequest request, String name) {
+		Cookie[] cks = request.getCookies();
+		if (cks == null) {
+			return null;
+		}
+		for (Cookie cookie : cks) {
+			if (name.equals(cookie.getName())) {
+				return cookie.getValue();
+			}
+		}
+		return null;
+
+	}
 }
