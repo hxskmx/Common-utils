37d4f50035e2abead0d10eb51b8490c53e1f9955
hxskmx <hxskmx@163.com>
2015-02-02 09:40:07 +0800

    add Verifycode

--- src/main/java/cn/com/lc/common/utils/CommonUtils.java
+++ src/main/java/cn/com/lc/common/utils/CommonUtils.java
@@ -1,7 +1,11 @@
 package cn.com.lc.common.utils;
 
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.beanutils.BeanUtils;
 
 /**
  * 开发工具类
@@ -11,6 +15,12 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class CommonUtils {
 
+	/**
+	 * 根据Cookie名称获取Cookie值
+	 * @param request HttpServletRequest
+	 * @param name
+	 * @return CookieValue
+	 */
 	public static String getCookieValue(HttpServletRequest request, String name) {
 		Cookie[] cks = request.getCookies();
 		if (cks == null) {
@@ -23,5 +33,20 @@ public class CommonUtils {
 		}
 		return null;
 
+	}
+	/**
+	 * 将表单参数封装Map映射到实体类中
+	 * @param map
+	 * @param clazz
+	 * @return
+	 */
+	public static <T> T toBean(Map map,Class<T> clazz ){
+		try{
+			T  bean = clazz.newInstance();
+			BeanUtils.populate(bean, map);
+			return bean;
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
 	}
 }
